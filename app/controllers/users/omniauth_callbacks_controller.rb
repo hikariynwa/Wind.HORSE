0c1dc6f7a4264a869a02863cbd200102659fc640
Hikari Osawa <hihnhikari0417@gmail.com>
2024-11-07 21:46:06 +0900

    ログイン後のredirect先の変更

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,8 +9,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def callback_for(provider)
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @user, event: :authentication
+    sign_in @user, event: :authentication
     set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+    redirect_to search_datas_path
   end
 
   def failure

8dd96b29da07cdf965143274406221781ded3dbf
Hikari Osawa <hihnhikari0417@gmail.com>
2024-11-07 22:22:39 +0900

    デバックメッセージを追加

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,11 +10,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def callback_for(provider)
     @user = User.from_omniauth(request.env["omniauth.auth"])
     sign_in @user, event: :authentication
+    Rails.logger.debug("#{@user}kokomite")
     set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
     redirect_to search_datas_path
   end
 
   def failure
+    flash[:alert] = "ログインに失敗しました"
     redirect_to root_path
   end
 end
